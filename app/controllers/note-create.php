03db3216fd03e4c1364946f528952581c00066ba
David Lombera <david.lomberlan@gmail.com>
2023-06-27 17:05:13 -0600

    EP 27 - S3 - Intro to Form Validation

--- app/controllers/note-create.php
+++ app/controllers/note-create.php
@@ -5,11 +5,25 @@ use App\Components\Database;
 $config = require __DIR__ . '/../../bootstrap/config.php';
 $db = new Database($config['database'], 'db', 'db');
 
+$errors = [];
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $db->query('INSERT INTO notes (user_id, body) VALUES(:user, :body);', [
-        'user' => 1,
-        'body' => $_POST['body'],
-    ]);
+    $body = $_POST['body'];
+
+    if (strlen($body) === 0) {
+        $errors['body'] = 'A body is required';
+    }
+
+    if (strlen($body) > 1000) {
+        $errors['body'] = 'The body can not be more than 1,000 characters';
+    }
+
+    if (count($errors) === 0) {
+        $db->query('INSERT INTO notes (user_id, body) VALUES(:user, :body);', [
+            'user' => 1,
+            'body' => $body,
+        ]);
+    }
 }
 
-render('note-create', ['heading' => 'Create Note']);
+render('note-create', ['heading' => 'Create Note', 'errors' => $errors]);
