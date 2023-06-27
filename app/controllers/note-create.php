f821904c2d71ff9d4826516a9a4d8301772c172f
David Lombera <david.lomberlan@gmail.com>
2023-06-27 16:36:34 -0600

    EP 26 - S3 - Always Escape Untrusted Input

--- app/controllers/note-create.php
+++ app/controllers/note-create.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Components\Database;
+
+$config = require __DIR__ . '/../../bootstrap/config.php';
+$db = new Database($config['database'], 'db', 'db');
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    dd($_POST);
+    $db->query('INSERT INTO notes (user_id, body) VALUES(:user, :body);', [
+        'user' => 1,
+        'body' => $_POST['body'],
+    ]);
 }
 
 render('note-create', ['heading' => 'Create Note']);
