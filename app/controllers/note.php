216e9497f87fa82826608de55c15b5fd3bf87ead
David Lombera <david.lomberlan@gmail.com>
2023-06-26 19:50:45 -0600

    EP 23 - S3 - Introduction to Authorization

--- app/controllers/note.php
+++ app/controllers/note.php
@@ -1,10 +1,23 @@
 <?php
 
 use App\Components\Database;
+use App\Components\Response;
 
 $config = require __DIR__ . '/../../bootstrap/config.php';
 $db = new Database($config['database'], 'db', 'db');
 
-$note = $db->query('SELECT * FROM notes WHERE id = :id;', ['id' => $_GET['id']])->fetch();
+$currentUserId = 1;
+
+$note = $db->query('SELECT * FROM notes WHERE id = :id;', [
+    'id' => $_GET['id'],
+])->fetch();
+
+if (!$note) {
+    abort();
+}
+
+if ($note->user_id !== $currentUserId) {
+    abort(Response::FORBIDDEN);
+}
 
 render('note', ['heading' => 'Note', 'note' => $note]);
--- index.php
+++ index.php
@@ -2,4 +2,5 @@
 
 require __DIR__ . '/bootstrap/helpers.php';
 require __DIR__ . '/src/Components/Database.php';
+require __DIR__ . '/src/Components/Response.php';
 require __DIR__ . '/bootstrap/router.php';
