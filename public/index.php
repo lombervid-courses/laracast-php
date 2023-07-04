e00b2c8648c0758ce4b8534f2097e71cf314435b
David Lombera <david.lomberlan@gmail.com>
2023-07-04 15:19:58 -0600

    EP 47 - S7 - Composer and Free Autoloading

--- public/index.php
+++ public/index.php
@@ -8,15 +8,11 @@ use Core\ValidationException;
 
 const BASE_PATH = __DIR__ . '/../';
 
-require BASE_PATH  . '/app/helpers.php';
+require BASE_PATH . 'vendor/autoload.php';
 
 session_start();
 
-spl_autoload_register(function ($class) {
-    $path = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-
-    require_once base_path("src/{$path}.php");
-});
+require BASE_PATH  . '/app/helpers.php';
 
 require base_path('bootstrap/app.php');
 
